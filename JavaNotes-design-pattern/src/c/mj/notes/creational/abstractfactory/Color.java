411baddcf0ee59fe6ecb9fc1e9f7a243940703d9
ChenMJ <616772117@qq.com>
2020-04-16 10:20:18 +0800

    fix 增加maven管理

--- JavaNotes-design-pattern/src/c/mj/notes/creational/abstractfactory/Color.java
+++
@@ -1,9 +0,0 @@
-package c.mj.notes.creational.abstractfactory;
-
-/**
- * @author ChenMJ
- * @version Color.class, v 0.1 2020/4/15 17:12  Exp$
- */
-public interface Color {
-    void fill();
-}
--- JavaNotes-design-pattern/src/c/mj/notes/creational/factory/Factory.java
+++
@@ -1,9 +0,0 @@
-package c.mj.notes.creational.factory;
-
-/**
- * @author ChenMJ
- * @version Factory.class, v 0.1 2020/4/15 15:27  Exp$
- */
-public interface Factory {
-    Product factoryMethod();
-}
